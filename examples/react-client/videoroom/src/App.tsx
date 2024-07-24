f3a86572708861bed1a7625a02a41d04fa42a28d
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-07-24 10:36:30 +0200

    fix styling

--- examples/react-client/videoroom/src/App.tsx
+++ examples/react-client/videoroom/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
                 />
               )}
 
-              <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs">
+              <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs z-30">
                 <p className="bg-slate-100/60 px-1 rounded-sm">You</p>
               </div>
             </div>
@@ -66,16 +66,11 @@ function App() {
 
             return (
               <div
-                className="aspect-video relative bg-zinc-300 rounded-md"
-                style={
-                  audioTrack?.vadStatus === "speech"
-                    ? {
-                        borderWidth: 4,
-                        borderColor: "red",
-                        boxSizing: "content-box",
-                      }
-                    : {}
-                }
+                className="aspect-video overflow-hidden grid place-content-center relative bg-zinc-300 rounded-md"
+                style={{
+                  outline:
+                    audioTrack?.vadStatus === "speech" ? "solid red 2px" : "",
+                }}
                 key={id}
               >
                 {audioTrack && <AudioPlayer stream={audioTrack.stream} />}
@@ -89,7 +84,7 @@ function App() {
                   />
                 )}
 
-                <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs">
+                <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs z-30">
                   <p className="bg-slate-100/60 px-1 rounded-sm">{id}</p>
                 </div>
               </div>

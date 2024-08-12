fishjam-cloud/web-client-sdk

create screenshare manager hook

packages/react-client/src/types.ts
@@ -1,8 +1,8 @@
-import type { ConnectConfig, SimulcastConfig, TrackBandwidthLimit } from "@fishjam-dev/ts-client";
-import type { ScreenShareManagerConfig } from "./ScreenShareManager";
+import type { ConnectConfig, Peer, SimulcastConfig, TrackBandwidthLimit } from "@fishjam-dev/ts-client";
 import type { PeerStatus, Selector, State, Track, TrackId, TrackWithOrigin, UseReconnection } from "./state.types";
 import type { JSX, ReactNode } from "react";
 import type { Client } from "./Client";
+import { useScreenShare } from "./screenShareTrackManager";
 
 export type DevicesStatus = "OK" | "Error" | "Not requested" | "Requesting";
 export type MediaStatus = "OK" | "Error" | "Not requested" | "Requesting";
@@ -125,8 +125,6 @@ export type UseSetupMediaConfig<TrackMetadata> = {
      */
     broadcastOnDeviceStart?: boolean;
 
-    streamConfig?: ScreenShareManagerConfig;
-
     defaultTrackMetadata?: TrackMetadata;
     defaultMaxBandwidth?: TrackBandwidthLimit;
   };
@@ -174,27 +172,9 @@ export type UserMediaAPI<TrackMetadata> = {
   devices: MediaDeviceInfo[] | null;
 };
 
-export type ScreenShareAPI<TrackMetadata> = {
-  initialize: (config?: ScreenShareManagerConfig) => Promise<void>;
-  cleanup: () => Promise<void>;
-  startStreaming: (trackMetadata?: TrackMetadata, maxBandwidth?: TrackBandwidthLimit) => Promise<string>;
-  stopStreaming: () => Promise<void>;
-  disableTrack: () => void;
-  enableTrack: () => void;
-  broadcast: Track<TrackMetadata> | null;
-  status: DevicesStatus | null;
-  stream: MediaStream | null;
-  track: MediaStreamTrack | null;
-  enabled: boolean;
-  // todo is mediaStatus necessary?,
-  mediaStatus: MediaStatus | null;
-  error: DeviceError | null;
-};
-
 export type Devices<TrackMetadata> = {
   camera: UserMediaAPI<TrackMetadata>;
   microphone: UserMediaAPI<TrackMetadata>;
-  screenShare: ScreenShareAPI<TrackMetadata>;
 };
 
 export type FishjamContextProviderProps = {
@@ -217,7 +197,7 @@ export type CreateFishjamClient<PeerMetadata, TrackMetadata> = {
   useSetupMedia: (config: UseSetupMediaConfig<TrackMetadata>) => UseSetupMediaResult;
   useCamera: () => Devices<TrackMetadata>["camera"] & GenericTrackManager<TrackMetadata>;
   useMicrophone: () => Devices<TrackMetadata>["microphone"] & GenericTrackManager<TrackMetadata>;
-  useScreenShare: () => ScreenShareAPI<TrackMetadata>;
   useClient: () => Client<PeerMetadata, TrackMetadata>;
   useReconnection: () => UseReconnection;
+  useScreenShare: () => ReturnType<typeof useScreenShare<PeerMetadata, TrackMetadata>>;
 };

packages/react-client/src/utils/tracks.ts
@@ -1,0 +1,41 @@
+import { FishjamClient, TrackContext } from "@fishjam-dev/ts-client";
+import { Track } from "../state.types";
+
+export const getRemoteTrackContext = <PeerMetadata, TrackMetadata>(
+  tsClient: FishjamClient<PeerMetadata, TrackMetadata>,
+  remoteOrLocalTrackId: string | null,
+): TrackContext<PeerMetadata, TrackMetadata> | null => {
+  if (!remoteOrLocalTrackId) return null;
+
+  const tracks = tsClient?.getLocalEndpoint()?.tracks;
+  if (!tracks) return null;
+
+  const trackByRemoteId = tracks?.get(remoteOrLocalTrackId);
+  if (trackByRemoteId) return trackByRemoteId;
+
+  const trackByLocalId = [...tracks.values()].find((track) => track.track?.id === remoteOrLocalTrackId);
+  return trackByLocalId ? trackByLocalId : null;
+};
+
+export const getTrackFromContext = <TrackMetadata>(
+  track: TrackContext<unknown, TrackMetadata>,
+): Track<TrackMetadata> => ({
+  rawMetadata: track.rawMetadata,
+  metadata: track.metadata,
+  trackId: track.trackId,
+  stream: track.stream,
+  simulcastConfig: track.simulcastConfig || null,
+  encoding: track.encoding || null,
+  vadStatus: track.vadStatus,
+  track: track.track,
+  metadataParsingError: track.metadataParsingError,
+});
+
+export const getRemoteTrack = <PeerMetadata, TrackMetadata>(
+  tsClient: FishjamClient<PeerMetadata, TrackMetadata>,
+  remoteOrLocalTrackId: string | null,
+) => {
+  const context = getRemoteTrackContext(tsClient, remoteOrLocalTrackId);
+  if (!context) return null;
+  return getTrackFromContext(context);
+};

fishjam-cloud/web-sdk

packages/react-client/src/types.ts
@@ -1,5 +1,4 @@
-import type { ConnectConfig, SimulcastConfig, TrackBandwidthLimit } from "@fishjam-cloud/ts-client";
-import type { ScreenShareManagerConfig } from "./ScreenShareManager";
+import type { ConnectConfig, Peer, SimulcastConfig, TrackBandwidthLimit } from "@fishjam-cloud/ts-client";
 import type {
   PeerState,
   PeerStatus,
@@ -12,6 +11,7 @@ import type {
 } from "./state.types";
 import type { JSX, ReactNode } from "react";
 import type { Client } from "./Client";
+import { useScreenShare } from "./screenShareTrackManager";
 
 export type DevicesStatus = "OK" | "Error" | "Not requested" | "Requesting";
 export type MediaStatus = "OK" | "Error" | "Not requested" | "Requesting";
@@ -134,8 +134,6 @@ export type UseSetupMediaConfig<TrackMetadata> = {
      */
     broadcastOnDeviceStart?: boolean;
 
-    streamConfig?: ScreenShareManagerConfig;
-
     defaultTrackMetadata?: TrackMetadata;
     defaultMaxBandwidth?: TrackBandwidthLimit;
   };
@@ -183,27 +181,9 @@ export type UserMediaAPI<TrackMetadata> = {
   devices: MediaDeviceInfo[] | null;
 };
 
-export type ScreenShareAPI<TrackMetadata> = {
-  initialize: (config?: ScreenShareManagerConfig) => Promise<void>;
-  cleanup: () => Promise<void>;
-  startStreaming: (trackMetadata?: TrackMetadata, maxBandwidth?: TrackBandwidthLimit) => Promise<string>;
-  stopStreaming: () => Promise<void>;
-  disableTrack: () => void;
-  enableTrack: () => void;
-  broadcast: Track<TrackMetadata> | null;
-  status: DevicesStatus | null;
-  stream: MediaStream | null;
-  track: MediaStreamTrack | null;
-  enabled: boolean;
-  // todo is mediaStatus necessary?,
-  mediaStatus: MediaStatus | null;
-  error: DeviceError | null;
-};
-
 export type Devices<TrackMetadata> = {
   camera: UserMediaAPI<TrackMetadata>;
   microphone: UserMediaAPI<TrackMetadata>;
-  screenShare: ScreenShareAPI<TrackMetadata>;
 };
 
 export type FishjamContextProviderProps = {
@@ -234,11 +214,11 @@ export type CreateFishjamClient<PeerMetadata, TrackMetadata> = {
   useSetupMedia: (config: UseSetupMediaConfig<TrackMetadata>) => UseSetupMediaResult;
   useCamera: () => Devices<TrackMetadata>["camera"] & GenericTrackManager<TrackMetadata>;
   useMicrophone: () => Devices<TrackMetadata>["microphone"] & GenericTrackManager<TrackMetadata>;
-  useScreenShare: () => ScreenShareAPI<TrackMetadata>;
   useClient: () => Client<PeerMetadata, TrackMetadata>;
   useReconnection: () => UseReconnection;
   useParticipants: () => {
     localParticipant: PeerStateWithTracks<PeerMetadata, TrackMetadata> | null;
     participants: PeerStateWithTracks<PeerMetadata, TrackMetadata>[];
   };
+  useScreenShare: () => ReturnType<typeof useScreenShare<PeerMetadata, TrackMetadata>>;
 };

packages/react-client/src/utils/tracks.ts
@@ -1,0 +1,41 @@
+import { FishjamClient, TrackContext } from "@fishjam-cloud/ts-client";
+import { Track } from "../state.types";
+
+export const getRemoteTrackContext = <PeerMetadata, TrackMetadata>(
+  tsClient: FishjamClient<PeerMetadata, TrackMetadata>,
+  remoteOrLocalTrackId: string | null,
+): TrackContext<PeerMetadata, TrackMetadata> | null => {
+  if (!remoteOrLocalTrackId) return null;
+
+  const tracks = tsClient?.getLocalPeer()?.tracks;
+  if (!tracks) return null;
+
+  const trackByRemoteId = tracks?.get(remoteOrLocalTrackId);
+  if (trackByRemoteId) return trackByRemoteId;
+
+  const trackByLocalId = [...tracks.values()].find((track) => track.track?.id === remoteOrLocalTrackId);
+  return trackByLocalId ? trackByLocalId : null;
+};
+
+export const getTrackFromContext = <TrackMetadata>(
+  track: TrackContext<unknown, TrackMetadata>,
+): Track<TrackMetadata> => ({
+  rawMetadata: track.rawMetadata,
+  metadata: track.metadata,
+  trackId: track.trackId,
+  stream: track.stream,
+  simulcastConfig: track.simulcastConfig || null,
+  encoding: track.encoding || null,
+  vadStatus: track.vadStatus,
+  track: track.track,
+  metadataParsingError: track.metadataParsingError,
+});
+
+export const getRemoteTrack = <PeerMetadata, TrackMetadata>(
+  tsClient: FishjamClient<PeerMetadata, TrackMetadata>,
+  remoteOrLocalTrackId: string | null,
+) => {
+  const context = getRemoteTrackContext(tsClient, remoteOrLocalTrackId);
+  if (!context) return null;
+  return getTrackFromContext(context);
+};
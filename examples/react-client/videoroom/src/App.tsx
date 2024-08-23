9b901c30f6afa29d35228ad290122fbac953bb4c
Miron Pawlik <miron.pawlik@swmansion.com>
2024-08-23 15:01:01 +0200

    Refactor code to extract Video tracks

--- examples/react-client/videoroom/src/App.tsx
+++ examples/react-client/videoroom/src/App.tsx
@@ -4,6 +4,36 @@ import { DevicePicker } from "./DevicePicker";
 import { RoomConnector } from "./RoomConnector";
 import VideoPlayer from "./VideoPlayer";
 import AudioPlayer from "./AudioPlayer";
+import { Track } from "@fishjam-cloud/react-client";
+
+function VideoTracks({
+  videoTracks,
+  name,
+  id,
+}: {
+  videoTracks: Track<unknown>[] | undefined;
+  name: string;
+  id: string;
+}) {
+  return videoTracks?.map((videoTrack) => (
+    <div
+      className="aspect-video overflow-hidden grid place-content-center relative bg-zinc-300 rounded-md"
+      key={videoTrack.trackId}
+    >
+      {videoTrack && (
+        <VideoPlayer className="rounded-md z-20" key={videoTrack.trackId} stream={videoTrack.stream} peerId={id} />
+      )}
+
+      <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs z-30">
+        <p className="bg-slate-100/60 px-1 rounded-sm">{name}</p>
+      </div>
+    </div>
+  ));
+}
+
+function AudioTracks({ audioTracks }: { audioTracks: Track<unknown>[] | undefined }) {
+  return audioTracks?.map((audioTrack) => <AudioPlayer stream={audioTrack.stream} key={audioTrack.trackId} />);
+}
 
 function App() {
   const client = useClient();
@@ -15,7 +45,6 @@ function App() {
       audioTrackConstraints: true,
     });
   }, [client]);
-
   return (
     <main className="w-screen h-screen flex">
       <section className="w-1/3 bg-zinc-200 p-4 h-full space-y-8">
@@ -28,52 +57,14 @@ function App() {
 
       <div className="w-full h-full p-4">
         <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          {localParticipant && (
-            <div className="aspect-video overflow-hidden grid place-content-center bg-zinc-300 rounded-md relative">
-              {localParticipant.videoTrack && (
-                <VideoPlayer
-                  className="rounded-md z-20"
-                  key={localParticipant.videoTrack.trackId}
-                  stream={localParticipant.videoTrack.stream}
-                  peerId={"0"}
-                />
-              )}
+          {localParticipant && <VideoTracks videoTracks={localParticipant.videoTracks} name="You" id="0" />}
 
-              <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs z-30">
-                <p className="bg-slate-100/60 px-1 rounded-sm">You</p>
-              </div>
-            </div>
-          )}
-
-          {participants.map(({ id, audioTrack, videoTrack, metadata }) => {
-            return (
-              <div
-                className="aspect-video overflow-hidden grid place-content-center relative bg-zinc-300 rounded-md"
-                style={{
-                  outline:
-                    audioTrack?.vadStatus === "speech" ? "solid red 2px" : "",
-                }}
-                key={id}
-              >
-                {audioTrack && <AudioPlayer stream={audioTrack.stream} />}
-
-                {videoTrack && (
-                  <VideoPlayer
-                    className="rounded-md z-20"
-                    key={videoTrack.trackId}
-                    stream={videoTrack.stream}
-                    peerId={id}
-                  />
-                )}
-
-                <div className="absolute bottom-2 left-0 w-full grid place-content-center text-center text-xs z-30">
-                  <p className="bg-slate-100/60 px-1 rounded-sm">
-                    {(metadata as { name?: string })?.name ?? id}
-                  </p>
-                </div>
-              </div>
-            );
-          })}
+          {participants.map(({ id, videoTracks, metadata }) => (
+            <VideoTracks videoTracks={videoTracks} name={(metadata as { name?: string })?.name ?? id} id={id} />
+          ))}
+          {participants.map(({ id, audioTracks }) => (
+            <AudioTracks audioTracks={audioTracks} key={id} />
+          ))}
         </section>
       </div>
     </main>

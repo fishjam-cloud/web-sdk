f8fe9a04f87f85a06e526747973179cf7a6193f8
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-09-20 14:13:29 +0200

    FCE-550 Visualise audio props (#108)
    ## Description
    
    
    ![image](https://github.com/user-attachments/assets/ba86c4fe-b6c0-45aa-b726-08fa8dcde7ec)
    
    Every tile on `fishjam-chat` displays:
    
    - Audio waveform
    - Muted indicator: 🔇 or 🔊
    - Speaking indicator: 🗣 or 🤐
    
    It is also possible to identify the audio origin (participant) and
    source (screen share vs. microphone).
    
    ## Motivation and Context
    
    To be able to visualise audio streams.
    
    ## Types of changes
    
    New feature (non-breaking change which adds functionality)
    
    ---------
    
    Co-authored-by: Miron Pawlik <mironiasty@users.noreply.github.com>

--- examples/react-client/fishjam-chat/src/components/Tile.tsx
+++ examples/react-client/fishjam-chat/src/components/Tile.tsx
@@ -1,30 +1,49 @@
 import VideoPlayer from "./VideoPlayer";
 import { Track } from "@fishjam-cloud/react-client";
+import AudioVisualizer from "./AudioVisualizer";
 
 type Props = {
   id: string;
   name: string;
-  videoTrack: Track | undefined;
+  videoTrack?: Track;
+  audioTrack?: Track;
 };
 
-export function Tile({ videoTrack, name, id }: Props) {
+export function Tile({ videoTrack, audioTrack, name, id }: Props) {
+  const isMuted = !audioTrack || audioTrack.metadata?.paused;
+  const isSpeaking = audioTrack?.vadStatus === "speech";
+
   return (
     <div className="relative grid aspect-video place-content-center overflow-hidden rounded-md bg-zinc-300">
       {videoTrack && (
         <VideoPlayer
           className="z-20 rounded-md"
-          peerId={id}
           stream={videoTrack.stream}
+          peerId={id}
         />
       )}
 
       <div className="absolute bottom-2 left-0 z-30 grid w-full place-content-center text-center text-xs">
-        <span
+        <AudioVisualizer stream={audioTrack?.stream} />
+
+        <div
           title={videoTrack?.trackId}
-          className="rounded-sm bg-slate-100/60 px-1"
+          className="flex justify-between rounded-sm bg-slate-100/60 px-1"
         >
-          {name}
-        </span>
+          {isMuted ? (
+            <span title="Muted">🔇</span>
+          ) : (
+            <span title="Unmuted">🔊</span>
+          )}
+
+          <span>{name}</span>
+
+          {isSpeaking ? (
+            <span title="Speaking">🗣</span>
+          ) : (
+            <span title="Silent">🤐</span>
+          )}
+        </div>
       </div>
     </div>
   );

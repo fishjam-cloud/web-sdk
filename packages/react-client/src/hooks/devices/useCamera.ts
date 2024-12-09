2c7a4a540a6b602062cfcebc1a626155011f8f15
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-12-09 11:11:41 +0100

    add typedoc

--- packages/react-client/src/hooks/devices/useCamera.ts
+++ packages/react-client/src/hooks/devices/useCamera.ts
@@ -11,14 +11,37 @@ export function useCamera(): CameraApi {
   const deviceApi = useDeviceApi({ deviceManager: videoDeviceManagerRef.current });
 
   return {
+    /** Toggles current camera on/off */
     toggleCamera: videoTrackManager.toggleDevice,
+    /** Selects the camera device  */
     selectCamera: videoTrackManager.selectDevice,
+    /**
+     * Indicates which camera is now turned on and streaming
+     */
     activeCamera: deviceApi.activeDevice,
+    /**
+     * Indicates whether the microphone is streaming video
+     */
     isCameraOn: !!deviceApi.mediaStream,
+    /**
+     * The MediaStream object containing the current stream
+     */
     cameraStream: deviceApi.mediaStream,
+    /**
+     * The currently set camera middleware function
+     */
     currentCameraMiddleware: deviceApi.currentMiddleware,
+    /**
+     * Sets the camera middleware
+     */
     setCameraTrackMiddleware: videoTrackManager.setTrackMiddleware,
+    /**
+     * List of available camera devices
+     */
     cameraDevices: deviceApi.devices,
+    /**
+     * Possible error thrown while setting up the camera
+     */
     cameraDeviceError: deviceApi.deviceError,
   };
 }
--- packages/react-client/src/hooks/devices/useMicrophone.ts
+++ packages/react-client/src/hooks/devices/useMicrophone.ts
@@ -11,16 +11,43 @@ export function useMicrophone(): MicrophoneApi {
   const deviceApi = useDeviceApi({ deviceManager: audioDeviceManagerRef.current });
 
   return {
+    /** Toggles current microphone on/off */
     toggleMicrophone: audioTrackManager.toggleDevice,
+    /** Mutes/unmutes the microphone */
     toggleMicrophoneMute: audioTrackManager.toggleMute,
+    /** Selects the microphone device */
     selectMicrophone: audioTrackManager.selectDevice,
+    /**
+     * Indicates which microphone is now turned on and streaming audio
+     */
     activeMicrophone: deviceApi.activeDevice,
+    /**
+     * Indicates whether the microphone is streaming audio
+     */
     isMicrophoneOn: !!deviceApi.mediaStream,
+    /**
+     * Indicates whether the microphone is muted
+     */
     isMicrophoneMuted: audioTrackManager.paused,
+    /**
+     * The MediaStream object containing the current audio stream
+     */
     microphoneStream: deviceApi.mediaStream,
+    /**
+     * The currently set microphone middleware function
+     */
     currentMicrophoneMiddleware: deviceApi.currentMiddleware,
+    /**
+     * Sets the microphone middleware
+     */
     setMicrophoneTrackMiddleware: audioTrackManager.setTrackMiddleware,
+    /**
+     * List of available microphone devices
+     */
     microphoneDevices: deviceApi.devices,
+    /**
+     * Possible error thrown while setting up the microphone
+     */
     microphoneDeviceError: deviceApi.deviceError,
   };
 }
